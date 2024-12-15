b2dc6b98df7e02e61dd473a55885fd7f4c8f055e
Meherrem ALiyev <maharram.aliyev98@gmail.com>
2024-12-15 19:32:39 +0400

    signup page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,29 @@
 const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+const selectElements = document.querySelectorAll('.dynamic-select');
+
 
 const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
 });
+
+document.querySelectorAll('.numeric-input').forEach(input => {
+  input.addEventListener('input', function () {
+      this.value = this.value.replace(/[^0-9]/g, '');
+  });
+});
+
+
+selectElements.forEach(selectElement => {
+    function updateOptionsVisibility() {
+        const selectedIndex = selectElement.selectedIndex;
+        Array.from(selectElement.options).forEach((option, index) => {
+            option.style.display = index === selectedIndex ? 'none' : 'block';
+        });
+    }
+
+    updateOptionsVisibility();
+
+    selectElement.addEventListener('change', updateOptionsVisibility);
+});
+
+
