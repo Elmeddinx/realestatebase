f83f49fe8d0487f87636bcd0b28f960bcad22778
Meherrem ALiyev <maharram.aliyev98@gmail.com>
2025-01-27 04:41:25 +0400

    multiple select added and btn on form-container styled

--- assets/js/images.js
+++ assets/js/images.js
@@ -7,75 +7,82 @@ let uploadedImages = [];
 
 fileInput.addEventListener("change", function () {
     const files = Array.from(fileInput.files);
-  
-    files.forEach((file) => {
-        if (uploadedImages.length < 4) {
-            const reader = new FileReader();
-    
-            reader.onload = (e) => {
-                const imagePreview = document.createElement("div");
-                imagePreview.classList.add("image-preview");
-    
-                imagePreview.innerHTML = `
-                    <img src="${e.target.result}" alt="Yüklenen Şəkil">
-                    <div class="image-actions">
-                        <button class="delete">
-                            <img src="./assets/icons/delete-icon.svg" alt="Delete">
-                        </button>
-                        <button class="turn">
-                            <img class="turn-img" src="./assets/icons/turn-icon.svg" alt="Turn">
-                        </button>
-                        <button class="zoom">
-                            <img src="./assets/icons/zoom-icon.svg" alt="Zoom">
-                        </button>
-                    </div>
-                `;
-    
-                imagePreview.querySelector(".delete").addEventListener("click", () => {
-                    imagePreview.remove();
-                    uploadedImages = uploadedImages.filter((img) => img !== imagePreview);
-                    checkImageLimit();
-                });
-    
-                let rotation = 0;
-                imagePreview.querySelector(".turn").addEventListener("click", () => {
-                    rotation += 90;
-                    imagePreview.querySelector("img").style.transform = `rotate(${rotation}deg)`;
 
-                    const zoomedImage = document.getElementById('zoomedImage');
-                    zoomedImage.style.transform = `rotate(${rotation}deg)`;
-                });
-    
-                imagePreview.querySelector(".zoom").addEventListener("click", () => {
-                    const zoomedImage = document.getElementById('zoomedImage');
-                    zoomedImage.src = e.target.result;
-    
-                    const imageModal = new bootstrap.Modal(document.getElementById('imageModal'));
-                    imageModal.show();
-                });
-    
-                uploadedImages.push(imagePreview);
-                imagePreviewContainer.insertBefore(imagePreview, imageAddBox);
+    files.forEach((file) => {
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const imagePreview = document.createElement("div");
+            imagePreview.classList.add("image-preview");
+
+            imagePreview.innerHTML = `
+                <img src="${e.target.result}" alt="Yüklənən Şəkil">
+                <div class="image-actions">
+                    <button class="delete">
+                        <img src="./assets/icons/delete-icon.svg" alt="Delete">
+                    </button>
+                    <button class="turn">
+                        <img class="turn-img" src="./assets/icons/turn-icon.svg" alt="Turn">
+                    </button>
+                    <button class="zoom">
+                        <img src="./assets/icons/zoom-icon.svg" alt="Zoom">
+                    </button>
+                </div>
+            `;
+
+            imagePreview.querySelector(".delete").addEventListener("click", () => {
+                imagePreview.remove();
+                uploadedImages = uploadedImages.filter((img) => img !== imagePreview);
                 checkImageLimit();
-            };
-    
-            reader.readAsDataURL(file);
-        }
+            });
+
+            let rotation = 0;
+            imagePreview.querySelector(".turn").addEventListener("click", () => {
+                rotation += 90;
+                imagePreview.querySelector("img").style.transform = `rotate(${rotation}deg)`;
+
+                const zoomedImage = document.getElementById("zoomedImage");
+                zoomedImage.style.transform = `rotate(${rotation}deg)`;
+            });
+
+            imagePreview.querySelector(".zoom").addEventListener("click", () => {
+                const previewImg = imagePreview.querySelector("img");
+                const zoomedImage = document.getElementById("zoomedImage");
+                
+                zoomedImage.src = previewImg.src;
+
+                zoomedImage.style.transform = previewImg.style.transform;
+
+                const imageModal = new bootstrap.Modal(
+                    document.getElementById("imageModal")
+                );
+                imageModal.show();
+            });
+
+            uploadedImages.push(imagePreview);
+            imagePreviewContainer.insertBefore(imagePreview, imageAddBox);
+
+            checkImageLimit();
+        };
+
+        reader.readAsDataURL(file);
     });
-  
+
     fileInput.value = "";
 });
 
 function checkImageLimit() {
+    if (uploadedImages.length >= 4) {
+        uploadHint.style.display = "none";
+    } else {
+        uploadHint.style.display = "block";
+    }
+
     if (uploadedImages.length > 0) {
         imageAddBox.classList.remove("large");
         imageAddBox.classList.add("small");
-
-        uploadHint.style.display = "none";
     } else {
         imageAddBox.classList.remove("small");
         imageAddBox.classList.add("large");
-
-        uploadHint.style.display = "block";
     }
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,37 @@ document.querySelectorAll('.numeric-input').forEach(input => {
 });
 
 
+$(document).ready(function() {
+  $('.js-example-basic-multiple').select2({
+    templateResult: function (data) {
+      if (data.loading) return data.text;
+      if (!data.element) return data.text;
+  
+      const $element = $(data.element);
+      const isSelected = $element.prop('selected');
+  
+      const $label = $('<span>').text(data.text);
+
+      if (isSelected) {
+        $label.css({
+          display: 'inline-block',
+          width: '100%',
+          position: 'relative'
+        });
+        
+        const $tick = $('<span>').text('◉').css({
+          float: 'right',
+          color: 'black'
+        });
+
+        $label.append($tick);
+      }
+      
+      return $label;
+    }
+  });
+});
+
 
 
 
